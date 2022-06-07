59717bf63b36d917a845bb6239dd66284cfb9793
ln-dev7 <leonelngoya@gmail.com>
2022-06-07 14:21:19 +0100

    Header bottom responsive

--- scripts/header-bottom.js
+++ scripts/header-bottom.js
@@ -1,19 +1,30 @@
 const linkItems = document.querySelectorAll(".header-bottom-links .arrow");
+
 linkItems.forEach((element) => {
   element.addEventListener("click", function (e) {
     for (let index = 0; index < linkItems.length; index++) {
-      linkItems[index].classList.remove("active");
+      if (linkItems[index] !== e.target) {
+        linkItems[index].classList.remove("show");
+      }
     }
-    e.target.classList.toggle("active");
+    e.target.classList.toggle("show");
   });
-});
+})
 
 window.onclick = function (e) {
   if (!e.target.matches(".header-bottom-links .arrow")) {
     linkItems.forEach(element => {
-        if (element.classList.contains("active")) {
-            element.classList.remove("active");
+        if (element.classList.contains("show")) {
+            element.classList.remove("show");
           }
     });
   }
 }
+
+const burger = document.querySelector(".header-bottom-burger");
+const menuLinks = document.querySelector(".header-bottom-links");
+
+burger.addEventListener("click", (e) => {
+    e.preventDefault();
+    menuLinks.classList.toggle("show-menu");
+})
